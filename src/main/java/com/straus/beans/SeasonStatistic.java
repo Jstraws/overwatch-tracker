b564b8f70b76453dee7c198de2f02f670e70da77
Jeremy Straus <jstraws@verizon.net>
2019-05-15 15:14:03 -0500

    Whoops

--- src/main/java/com/straus/beans/SeasonStatistic.java
+++ src/main/java/com/straus/beans/SeasonStatistic.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SeasonStatistic extends Statistic {
-    List<Statistic> heroStats;
+    private List<Statistic> heroStats;
 
     public SeasonStatistic(String name) {
         super(name);
