9ad57140edaca6187252ec773d645922d2c3fc9f
Jeremy Straus <jstraws@verizon.net>
2019-05-28 15:06:01 -0500

    Add last 30 days matches played

--- src/main/java/com/straus/controllers/MatchController.java
+++ src/main/java/com/straus/controllers/MatchController.java
@@ -127,6 +127,7 @@ public class MatchController {
         try {
             return new ResponseEntity<>(matchService.getActivityList(id), HttpStatus.OK);
         } catch (Exception e) {
+            e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
